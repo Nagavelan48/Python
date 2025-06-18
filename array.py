7901c6a881eb19010920637dcf7b4a77457d583c
Nagavelan <nagavelan.chandramohan@multicorewareinc.com>
2025-06-18 23:28:22 +0530

    learn basic syntax

---
+++ array.py
@@ -0,0 +1,11 @@
+arr = [10,20,30,40]
+for i in range(len(arr)):
+    print(arr[i])
+
+arr.reverse()
+print(arr)
+
+
+t = (10,20,30,10,30,20,20,40,10)
+print(t.count(10))
+print(t.index(40))
---
+++ loop.py
@@ -0,0 +1,12 @@
+for i in range(6):
+    print(i)
+
+
+for i in range(1,12,3):
+    print(i)
+
+
+i = 3
+while i<5:
+    print(i*2)
+    i+=1
